5ab476f47a953286de6c786081d9a9ca7363bab0
Yuri Lima <61335084+yurilima7@users.noreply.github.com>
2023-01-28 18:28:30 -0300

    Home pt 1

--- lib/app/views/home_screen/home.dart
+++ lib/app/views/home_screen/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:pomodoro/app/controller/user_controller_impl.dart';
-import 'package:provider/provider.dart';
+import 'package:pomodoro/app/core/styles/colors_styles.dart';
+import 'package:pomodoro/app/core/styles/text_styles.dart';
+import 'package:pomodoro/app/core/widgets/main_card.dart';
+import 'package:pomodoro/app/core/widgets/outlined_card.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -8,9 +10,119 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Consumer<UserControllerImpl>(
-        builder: (context, user, child) => Center(
-          child: Text(user.user),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+      
+          child: LayoutBuilder(
+            builder: (_, constraints) {
+              return SingleChildScrollView(
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(minHeight: constraints.maxHeight),
+      
+                  child: IntrinsicHeight(
+                    child: Column(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(
+                            top: constraints.maxHeight * .025,
+                            bottom: constraints.maxHeight * .06,
+                          ),
+                          
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            
+                            children: [
+                              Row(
+                                children: [
+                                  Icon(
+                                    Icons.account_circle_outlined,
+                                    size: 50,
+                                    color: ColorsStyles.white,
+                                  ),
+
+                                  const SizedBox(width: 5,),
+
+                                  Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        'Nome',
+                                        style: TextStyles().titleSecondary,
+                                      ),
+                                      Text(
+                                        'Que seu dia seja muito produtivo!',
+                                        style: TextStyles().litteText,
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+
+                              IconButton(
+                                onPressed: () {},
+                                icon: Icon(Icons.more_vert, size: 32, color: ColorsStyles.white,),
+                              ),
+                            ],
+                          ),
+                        ),
+
+                        Container(
+                          margin: EdgeInsets.only(bottom: constraints.maxHeight * .06),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                
+                                children: [
+                                  Text(
+                                    'Atividade atual',
+                                    style: TextStyles().titleTertiary,
+                                  ),
+                                  Text(
+                                    'Ver todas',
+                                    style: TextStyles().litteTextBold,
+                                  ),
+                                ],
+                              ),
+
+                              const OutlinedCard(title: 'title', subtitle: 'subtitle'),
+                            ],
+                          ),
+                        ),
+
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+
+                          children: [
+                            Text(
+                              'Atividades do dia',
+                              style: TextStyles().titleTertiary,
+                            ),
+
+                            SizedBox(
+                              height: 320,
+                              child: ListView.builder(
+                                physics:const NeverScrollableScrollPhysics(),
+                                shrinkWrap: true,
+                                itemCount: 4,
+                                itemBuilder: (_, i) => const MainCard(
+                                  title: 'title',
+                                  subtitle: 'subtitle',
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
         ),
       ),
     );
